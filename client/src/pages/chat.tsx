3d22247285b682bd1ac5cbed9f78c4a0ab951ec3
AB-Cobb <andrew@cobbs-web.com>
2020-02-25 01:52:53 -0500

    state

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -1,69 +1,49 @@
 import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+  } from 'react-router-dom';
+import App from '../App';
 
-export class Title extends Component {
-    constructor(props : any) {
-        super(props);
-        this.state = {
-          value: null,
-        };
-    }
-    render () {
-        return (
-            <header>
-                <h1><span id="green">the R</span>eact <span id="green">H</span>ack <span id="green">C</span>hat</h1>
-            </header>
-        )
-    }
+interface chatState{
+    userName : string
+    room : string
 }
 
-class Roomnav extends Component {
+class Chatpage extends Component <{}, chatState>{
     constructor(props : any) {
         super(props);
         this.state = {
-          value: null,
+            userName: "Anonymouse",
+            room : "general"
         };
     }
     render () {
+        let room = this.state.room;
+        let links = [room == "general" ? <li className="active"><a >General</a></li>:  <li><a onClick={() => this.setState({room : 'general'})} >General</a></li>,
+                    room == "coding" ?  <li className="active"><a>Coding</a ></li> : <li><a onClick={() => this.setState({room : 'coding'})} >Coding</a ></li>,
+                    room == "off" ? <li className="active"><a>Off Topic</a ></li>: <li><a onClick={() => this.setState({room : 'off'})}>Off Topic</a ></li>,
+                    room == "tech" ? <li className="active"><a>Tech Help</a></li>:   <li><a onClick={() => this.setState({room : 'tech'})}>Tech Help</a></li>]
         return (
-            <aside>
-                <ul className="rm-links" >
-                    <li><a>General</a></li>
-                    <li className="active"><a  >Coding</a ></li>
-                    <li><a>Off Topic</a ></li>
-                    <li><a>Tech Help</a></li>
-                </ul>
-            </aside>
-        )
-    }
-}
-
-class Chatbox extends Component {
-    constructor(props : any) {
-        super(props);
-        this.state = {
-          value: null,
-        };
-    }
-    render () {
-    return (
-            <section id="chatroom">
-                <section id="feedback"></section>
-            </section>
-        )
-    }
-}
-
-class Userinputs extends Component {
-    constructor(props : any) {
-        super(props);
-        this.state = {
-          value: null,
-        };
-      }
-
-    render () {
-        return (
-            <footer>
+            <div style = {{height:"100vh"}}>
+                <header>
+                    <h1><span id="green">the R</span>eact <span id="green">H</span>ack <span id="green">C</span>hat</h1>
+                </header>
+                <main>
+                <aside>
+                    <ul className="rm-links" >
+                        {links[0]}
+                        {links[1]}
+                        {links[2]}
+                        {links[3]}
+                    </ul>
+                </aside>
+                <section id="chatroom">
+                    <section id="feedback"></section>
+                </section>
+                </main>
+                <footer>
             <section id="user_input">
             <div>
                 <span> Posting as: </span>
@@ -77,26 +57,6 @@ class Userinputs extends Component {
             </section>
                 <p id="copy">&copy;2020 Andrew Cobb</p>
             </footer>
-        )
-    }
-}
-
-class Chatpage extends Component {
-    constructor(props : any) {
-        super(props);
-        this.state = {
-          value: null,
-        };
-    }
-    render () {
-        return (
-            <div style = {{height:"100vh"}}>
-                <Title />
-                <main>
-                    <Roomnav />
-                    <Chatbox />
-                </main>
-                <Userinputs />
             </div>
         )
     }
