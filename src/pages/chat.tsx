450cc1c34c88fe9258a21ae474252c4d01d20103
AB-Cobb <andrew@cobbs-web.com>
2020-02-24 22:23:14 -0500

    reacts a fucking hack

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -46,20 +46,9 @@ class Chatbox extends Component {
     }
     render () {
     return (
-        <main>
-            <aside>
-                <ul className="rm-links" >
-                    <li><a href="/general">Room 1</a></li>
-                    <li className="active"><a href="/tech" >Room 2</a></li>
-                    <li><a href="">Room 3</a></li>
-                    <li><a href="">Room 4</a></li>
-                </ul>
-            </aside>
-        
             <section id="chatroom">
                 <section id="feedback"></section>
             </section>
-        </main>
         )
     }
 }
@@ -103,8 +92,10 @@ class Chatpage extends Component {
         return (
             <div>
                 <Title />
-                <Roomnav />
-                <Chatbox />
+                <main>
+                    <Roomnav />
+                    <Chatbox />
+                </main>
                 <Userinputs />
             </div>
         )
