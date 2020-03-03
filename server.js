069a16fd2f36384b4466630d4068222b6c21ca20
Andrew Cobb <andrew@cobbs-web.com>
2020-03-03 15:07:46 -0500

    DB

--- server.js
+++ server.js
@@ -44,12 +44,14 @@ numuser = 0;
 
 function getPastMessages (room)
 {  
+    console.log("get messages")
     return Pastmessages.find({room : room}, (error, data) => {
+        console.log("hello from get past messages, DATA: " , data)
         if (error) {
             console.log(error);
             return null
         }
-        console.log("hello from get past messages, DATA: " , data)
+        
         return data
     });
 }
